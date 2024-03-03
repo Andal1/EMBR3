e1e3ccfe20788ad80191ecc9d89ad2d3bf94c57d
Andal1 <andal250102@gmail.com>
2024-03-03 13:18:29 +0530

    update changes

--- src/Components/BMR/OralSyrup/Equipment21.jsx
+++ src/Components/BMR/OralSyrup/Equipment21.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import Button from "@mui/material/Button";
 import './TableWithCheckedCheckbox.css'; // Import your CSS file
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+ 
+const theme = createTheme({
+    spacing: 8, // Spacing factor
+});
 
 const Equipment21 = ({orderId}) => {
+  const [isEditable, setIsEditable] = useState(false);
   const [data, setData] = useState([
     // { sno: 1, equipment: 'Dispensing booth', idnumber: 'WH/R20/E05' , isChecked: false },
     // { sno: 2, equipment: 'Dispensing booth', idnumber: 'WH/R20/E06', isChecked: false },
@@ -12,23 +19,36 @@ const Equipment21 = ({orderId}) => {
    //Add more data as needed
   ]);
   useEffect(() => {
-    fetch("http://localhost:5038/equipment2_1")
+    console.log("inside useEffect:"+orderId);
+    fetch("http://localhost:5038/equipment2_1/"+orderId)
       .then((res) => res.json())
       .then((data) => setData(data));
-  }, []);
+  }, [orderId]);
 
+  const handleUpdate = () => {
+    fetch("http://localhost:5038/updateEquipment2_1", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data)
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }
   const handleCheckboxChange = (sno) => {
-    setData((prevData) =>
-      prevData.map((item) =>
-        item.sno === sno ? { ...item, isChecked: !item.isChecked } : item
+    setData((data) =>
+      data.map((item, index) =>
+        index === sno ? { ...item, isChecked: !item.isChecked } : item
       )
     );
   };
 
   return (
-    <div>
       <div className="table-container">
-      {orderId}
         <table className="styled-table">
           <thead>
             <tr>
@@ -45,18 +65,29 @@ const Equipment21 = ({orderId}) => {
                 <td>{item.equipment}</td>
                 <td>{item.idnumber}</td>
                 <td>
-                  <input
+                { isEditable ? (
+                    <input
                     type="checkbox"
                     checked={item.isChecked}
-                    onChange={() => handleCheckboxChange(item.sno)}
+                    onChange={() => handleCheckboxChange(index)}
                   />
+                  ) : (
+                    <input
+                    type="checkbox"
+                    checked={item.isChecked}
+                  />
+                  )}
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>
+        </table><br />
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'right'}}>
+          <Button variant="contained" onClick={()=>{setIsEditable(true)}}>&nbsp;&nbsp; Edit &nbsp;&nbsp;</Button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button variant="contained" onClick={handleUpdate}>Update</Button>
+        </div>
       </div>
-    </div>
   );
 };
 
